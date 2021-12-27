11064c00e9dc5072ed45cff1ecd2d348f9dbf660
Faton Hoti <hotifaton@outlook.com>
2021-12-27 18:18:26 +0100

    Create base project files

---
+++ src/main/java/com/domain/congregentur/CongregenturApplication.java
@@ -0,0 +1,13 @@
+package com.domain.congregentur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongregenturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongregenturApplication.class, args);
+	}
+
+}
